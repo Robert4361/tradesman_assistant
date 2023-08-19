7b416ffe4c50fc0457001bae4760bd2fa850a908
Robert4361 <robert4361@gmail.com>
2023-08-19 17:07:28 +0200

    Add database seed bank list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+banks = Bank.create([{ name: "Addiko Bank d.d.", pid: 14036333877 },
+                     { name: "Agram banka d.d.", pid: 70663193635 },
+                     { name: "Banka Kovanica d.d.", pid: 33039197637 },
+                     { name: "Croatia banka d.d.", pid: 32247795989 }])
