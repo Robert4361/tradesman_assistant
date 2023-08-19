724ee1ca226e3bcf97388988e9be42c1ca1e6656
Robert4361 <robert4361@gmail.com>
2023-08-19 16:38:47 +0200

    Add bank actions and views for creating

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,30 @@
 class BanksController < ApplicationController
+  before_action :set_bank, only: [:edit, :update, :destroy]
+
   def index
     @banks = Bank.all
+  end
+
+  def new
+    @bank = Bank.new
+  end
+
+  def create
+    @bank = Bank.new(bank_params)
+    if @bank.save
+      redirect_to root_path, notice: "Bank was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_bank
+    @bank = Bank.find(params[:id])
+  end
+
+  def bank_params
+    params.require(:bank).permit(:name, :pid)
   end
 end
