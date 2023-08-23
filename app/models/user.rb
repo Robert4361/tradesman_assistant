1554a7552c2a558ff71bc1969b8b9dce5d91a468
Robert4361 <robert4361@gmail.com>
2023-08-23 10:46:26 +0200

    Add user attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :bank
+  validates :first_name, :last_name, :business_name, :business_description, :pid, :post_number, :business_name, :iban, :city, presence: true
+  validates :iban, :pid, uniqueness: true
+  validates :first_name, :last_name, :city, format: { with: /\A[a-zA-Zšđčćž]+\z/,
+                                                      message: "only allows letters" }
+  validates :pid, :post_number, numericality: true
 end
