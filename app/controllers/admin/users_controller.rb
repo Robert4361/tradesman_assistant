4bc6b3dffb883728a4191fdbb4b8eea1e339c8b1
Robert4361 <robert4361@gmail.com>
2023-08-23 13:45:39 +0200

    Add user activation view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,30 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :activate]
+
   def index
-    @users = User.all
+    if params[:activated] == "false"
+      @users = User.where(activated?: false)
+    else
+      @users = User.all
+    end
+  end
+
+  def show
+  end
+
+  def activate
+    if @user.update(activated: true)
+      respond_to do |format|
+        format.html { render @user, notice: "User successfully activated" }
+        format.turbo_stream { render turbo_stream: turbo_stream.update("activated",
+                                                                       "<p> Activation status: true </p>") }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :users
     resources :banks
+    put "/activate/:id", to: "users#activate", as: "activate_user"
   end
 end
