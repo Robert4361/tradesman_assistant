b5b91cf062e77cb367c1c3cd55864e42bd54686c
Robert4361 <robert4361@gmail.com>
2023-08-25 15:12:29 +0200

    Add mandatory admin account activation in devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   belongs_to :bank
+  has_many :measurements
+
   validates :first_name, :last_name, :business_name, :business_description, :pid, :post_number, :business_name, :iban, :city, presence: true
   validates :iban, :pid, uniqueness: true
   validates :first_name, :last_name, :city, format: { with: /\A[a-zA-Zšđčćž]+\z/,
                                                       message: "only allows letters" }
   validates :pid, :post_number, numericality: true
+
+
+  def active_for_authentication?
+    super && activated?
+  end
+
+  def inactive_message
+    activated? ? super : :not_activated
+  end
 end
