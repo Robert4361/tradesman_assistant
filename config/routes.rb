f5734b5af2da448e6ab51511f49dba26a76b33fe
Robert4361 <robert4361@gmail.com>
2023-08-25 15:29:53 +0200

    Add needed authentication for every part of site

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "measurements#index"
 
   namespace :admin do
     resources :users
     resources :banks
     put "/activate/:id", to: "users#activate", as: "activate_user"
+    root "users#index"
   end
 
   resources :measurements
