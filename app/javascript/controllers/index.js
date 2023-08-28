d71d64c458ae46da247bd9f6b0a4d40209f240d7
Robert4361 <robert4361@gmail.com>
2023-08-28 09:55:39 +0200

    Add stimulus package for creating nested forms

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,6 +6,9 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+import NestedForm from 'stimulus-rails-nested-form'
+Stimulus.register('nested-form', NestedForm)
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
