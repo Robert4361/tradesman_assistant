1049b27914ab10ddcdecb97d1bb86a09142c4e66
Robert4361 <robert4361@gmail.com>
2023-08-29 18:14:36 +0200

    Get clients per user instead of all

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,7 +3,7 @@ class ClientsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @clients = Client.all
+    @clients = current_user.clients.all
   end
 
   def show
